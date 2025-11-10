c43cfaa494f3e3937a7d8b0c72afd429542c5e61
LuigihRagauske <luigih.ragauske@gmail.com>
2025-11-10 15:32:26 +0000

    Adiciona arquivos do projeto CoinFlip

---
+++ Models/Salario.cs
@@ -0,0 +1,18 @@
+﻿// Salario.cs
+using System;
+
+namespace AppTeste.Models // Garanta que este namespace esteja correto (AppTeste.Models)
+{
+    public class Salario
+    {
+        // Propriedades usadas para o cálculo
+        public decimal ValorHora { get; set; }
+        public decimal HorasTrabalhadas { get; set; }
+
+        // Método chamado pelo ViewModel (CalcularSalario)
+        public decimal CalcularSalario()
+        {
+            return ValorHora * HorasTrabalhadas;
+        }
+    }
+}
